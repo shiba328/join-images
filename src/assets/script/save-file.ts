f934a8304754a4f78284da9a94c02992ed0c4dd0
shiba mitsue <mitue.okamoto@gmail.com>
2020-03-07 04:13:55 +0900

    add: 画像の追加 fix: SP時のdialog

--- src/assets/script/save-file.ts
+++ src/assets/script/save-file.ts
@@ -23,24 +23,53 @@ export default class SaveFile {
 
     previewBtn.addEventListener('click', (e) => {
       const node = <HTMLElement>document.querySelector('[jsname="main-canvas"]');
-      node.setAttribute("style", "background: #fff;");
 
-      htmlToImage.toJpeg(node)
+      node.style.background = "#fff";
+
+      htmlToImage.toPng(node)
       .then(function (dataUrl) {
+        node.style.background = "transparent";
+
+        const alert = document.createElement('div');
+        alert.classList.add('dialog-alert');
+
+        const attention = document.createElement('div');
+        alert.appendChild(attention);
+        attention.classList.add('attention');
+        attention.innerText = "※画像が表示されない場合は「閉じる」を押して再度保存してください。 ※SP版では元の画像の合計が大きいと画像が生成されない場合があいります。"
+
+        const announce = document.createElement('div');
+        alert.appendChild(announce);
+        announce.classList.add('announce');
+        announce.innerText = "画像を長押しで「写真」に保存";
+
+        const scale = document.createElement('div');
+        alert.appendChild(scale);
+        scale.classList.add('scale');
+        scale.innerText = "倍率: 80%";
+
         var img = new Image();
-        img.addEventListener("load", function() {
-          // drawImage を実行する文をここに置く
+        alert.appendChild(img);
+        
+        img.width = node.offsetWidth;
+        img.height = node.offsetHeight;
+
+        img.addEventListener("load", (e) => {
         }, false);
         img.src = dataUrl;
 
         const dialog = new Dialog();
-        const format = dialog._format('preview', img);
+        const format = dialog._format('preview', alert);
+        format.classList.add('preview');
+
         document.body.appendChild(format);
         //閉じたら削除
         const closeBtns = format.querySelectorAll('[jsname="dialog_close_btn"]');
         closeBtns.forEach(element => {
           element.addEventListener('click', () => {
             format.remove();
+            //
+            document.body.classList.remove('dialog-overlay');
           });
         });
       })
