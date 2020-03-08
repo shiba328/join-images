3440a8a1e2db99cb32b69469bfc89a8c126c2cbd
shiba mitsue <mitue.okamoto@gmail.com>
2020-03-09 04:34:55 +0900

    add: cookie set
    add: クッキー制御
    add: ヘルプの表示
    fix: リードミー修正

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import 'normalize.css';
 import './style.css';
 
+import Cookies = require("js-cookie");
+
+import FirstAccess from './assets/script/first-access';
 import DropDrag from './assets/script/drop-drag';
 import LayoutTools from './assets/script/layout-tools';
 import PreviewFile from './assets/script/preview-file';
 
+// if(!Cookies.get('FirstAccess')) {
+  new FirstAccess();
+// }
 new DropDrag();
 new LayoutTools();
 new PreviewFile();
