106cbe26fd3b552c35e25f5ed72b7da0f3a9194c
shiba mitsue <mitue.okamoto@gmail.com>
2020-03-13 11:09:56 +0900

    chore: global veriable

---
+++ src/assets/script/get-version.ts
@@ -0,0 +1,20 @@
+declare var CONST: any;
+
+export default class GetVersion {
+  constructor () {
+    this._getMessage().then((data) => {
+      console.log(data)
+    }).catch((error) => {
+      console.error(error);
+    });
+  }
+
+  private _getMessage() {
+    const res = fetch(CONST.getVerJsonUrl);
+    return res.then((response) => {
+      return response.json();
+    }).catch((error) => {
+      throw error;
+    });
+  }
+}
