bc51d89d95a597603ad017e6a5e559f9cbd015d8
shiba328 <mitue.okamoto@gmail.com>
2020-03-14 21:41:51 +0900

    fix: 保存buttonのeventを削除

--- src/assets/script/save-file.ts
+++ src/assets/script/save-file.ts
@@ -2,13 +2,21 @@ export default class SaveFile {
   constructor(src) {
     const saveBtn =  <HTMLElement>document.querySelector('[jsname="save-btn"]');
     // this.visitor = ua('UA-XXXX-XX');
-    saveBtn.addEventListener('click', (e) => {
+    const f = (e) => {
       saveBtn.setAttribute('disabled', 'disabled');
       const link = document.createElement('a');
       link.download = `compile-image_${new Date().getTime()}.png`;
       link.href = src;
       link.click();
       saveBtn.removeAttribute('disabled');
-    })
+    }
+    saveBtn.addEventListener('click', f);
+    //閉じたら削除
+    const closeBtns = document.querySelectorAll('[jsname="dialog_close_btn"]');
+    closeBtns.forEach(element => {
+      element.addEventListener('click', () => {
+        saveBtn.removeEventListener('click', f);
+      });
+    });
   }
 }
